d1f6d0c71470c2082d8b28b0a37455f8e0d6334d
yamashita <yamashita@fizz.works>
2016-01-05 23:49:36 +0900

    Jenkinsでのコマンドビルドテストするために、いったん仮プロジェクトコミット

---
+++ GItConnect/Assets/Editor/BuildEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Diagnostics;
+
+public class ScriptBatch 
+{
+	[MenuItem("MyTools/BuildExecute")]
+	public static void BuildExecute ()
+	{
+		UnityEngine.Debug.Log ("build");
+
+		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.WebPlayer );
+		string[] allScene = new string[EditorBuildSettings.scenes.Length];
+		int i = 0;
+		foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+			allScene[i] = scene.path;
+			i++;
+		}
+		
+		string errorMsg_Device = BuildPipeline.BuildPlayer(allScene,
+		                                                   "./StandaloneWindows64.exe",
+								                          BuildTarget.StandaloneWindows,
+		                                                   BuildOptions.None
+		                         );
+
+		if (string.IsNullOrEmpty(errorMsg_Device)){
+			
+		} else {
+			//エラー処理適当に		
+		}
+	}
+}
---
+++ GItConnect/Assets/Scripts/Warning.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warning : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
